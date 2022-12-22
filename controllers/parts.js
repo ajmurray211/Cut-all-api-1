666bc17e6958cc253997229c38c80852d48d602a
AJ Murray <aj.murr4y@gmail.com>
2022-12-22 08:42:45 -0500

    filtering optionations added

--- controllers/parts.js
+++ controllers/parts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const  Part = require('../models/part')
+const Part = require('../models/part')
 
 //Show all parts
 router.get('/', (req, res) => {
@@ -16,15 +16,27 @@ router.post('/', (req, res) => {
 })
 
 //Update one part by ID
-router.patch('/:id', (req, res) => {
-    Part.findByIdAndUpdate(req.params.id, req.body, {new:true})
-    .then((updatedPost) => res.status(201).json({updatedPost: updatedPost}))
+router.put('/:id', (req, res) => {
+    Part.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((updatedPost) => res.status(201).json({ updatedPost: updatedPost }))
 })
 
 //Delete a part by ID
 router.delete('/:id', (req, res) => {
     Part.findByIdAndDelete(req.params.id)
-    .then((updatedPost) => res.status(204).json({updatedPost: updatedPost}))
+        .then((updatedPost) => res.status(204).json({ updatedPost: updatedPost }))
+})
+
+router.get('/search', (req, res) => {
+    const { name, tool } = req.query
+    console.log(req.query)
+    if (name) {
+        Part.find({ name: { $regex: `${name}` } })
+            .then(data => res.status(200).json({ data: data }))
+    } else if (tool) {
+        Part.find({ tool: { $regex: `${tool}` } })
+            .then(data => res.status(200).json({ data: data }))
+    }
 })
 
 module.exports = router
